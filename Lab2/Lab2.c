006a198721a174e22be9f9fd1b9a2053b9c45c6f
Austin Marino <Austin Marino>
2019-01-25 14:34:18 -0800

    Changed how cwd and start work as well as locateDir

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -28,20 +28,20 @@ typedef struct node{
 }Node;
 
 //Function Declarations
-Node * createNode(char*, char);
-void dbname(char *); bool tokenize(char *);
-Node * locateDir(char*); void initialize();
+char *getBaseName(char *); char *getDirName(char *);
+Node *createNode(char*, char); Node *locateDir2(char *);
+void dbname(char*); Node *locateDir(char *);
+Node * traverseDir(char*); void initialize();
 int findCmd(char *); int mkdir(char *);
 int rmdir(char *);   int ls(char *);
 int cd(char *);      int pwd(char *);
 int creat(char *);   int rm(char *);
 int reload(char *);  int save(char *);
 int menu(char *);    int quit(char *);
+void displayNode(Node *);
 //Gloabal Variables
-Node *root, *cwd; // root and CWD (Current Working Directory) pointers
-char line[128]; // user input command line
-char command[16], pathname[64] = "ROOT"; // command and pathname strings
-char dname[64], bname[64]; // dirname and basename string holders
+Node *root, *cwd, *start; // root and CWD (Current Working Directory) pointers
+char pathname[64] = "~", cwPath[64] = "",  dname[64] = "", bname[64] = "";
 char *cmd[] = {"mkdir", "rmdir", "ls", "cd", "pwd", "creat", "rm",
 			   "reload", "save", "menu", "quit", NULL};
 int (*fptr[ ])(char *)={(int (*)())mkdir,rmdir,ls,cd,pwd,creat,rm,
@@ -50,26 +50,29 @@ int (*fptr[ ])(char *)={(int (*)())mkdir,rmdir,ls,cd,pwd,creat,rm,
 int main(void){
 	initialize(); //initialize root node of the file system tree
 	while(true){
-		char path[64];
+		cwd = start;
+		strcpy(cwPath, pathname);
+		char line[128] = "", path[64] = "", command[16] = "";
+		int index = 0; //used for command index
+		strcpy(dname, ""); strcpy(bname, ""); 
 		printf("%s$ ", pathname);
 		fgets(line, 128, stdin); //get user input line = [command pathname];
 		line[strlen(line)-1] = 0; // kill \n at end of line
 		sscanf(line, "%s %s", command, path); //identify the command & path name
+		if(strlen(path)){ dbname(path);}
+		printf("path: %s\tdname: %s\tbname: %s\n", path, dname, bname);
 		if(!strcmp(command, "quit")){
 			break;
 		}
-		int index = findCmd(command); //finds the command to execute
-		if(index == -1){
+		else if((index = findCmd(command)) == -1){//finds the command to execute
 			printf("%s: command not found\n", command);
 			continue;
 		}
-		//printf("Command: %s\nPath: %s\nIndex: %d\n", command, pathname, index);
-		dbname(path);
-		//printf("dname: %s\tbname: %s\n", dname, bname);
-		if(!tokenize(dname)){ //attempts to change the cwd
+		else if((cwd = locateDir(dname)) == NULL){ //attempts to find the specified path
 			printf("bash: %s: %s: No such file or directory\n", command, path);
 			continue;
 		}
+		printf("path: %s\tdname: %s\tbname: %s\n", path, dname, bname);
 		int r = fptr[index](bname); //executes the command
 	}
 	
@@ -82,9 +85,30 @@ void initialize(){
 	root->Sibling = NULL;
 	root->Type = 'D';
 	strcpy(root->Name, pathname);
-	cwd = root;
+	start = cwd = root;
 }
-Node * locateDir(char *name){
+char *getBaseName(char *path){
+	bool isDir = false;
+	char *temp = path; 
+	for(int i = 0; *temp != '\0'; i++){
+		if(*temp == '/'){ isDir = true; break;}
+		temp++;
+	}
+	if(isDir){return strrchr(path, '/');}
+	return path;
+}
+char *getDirName(char *path){
+	int index = -1;
+	char *temp = path; 
+	for(int i = 0; *temp != '\0'; i++){
+		if(*temp == '/'){ index = i;}
+		temp++;
+	}
+	if(index != -1){return strncpy(temp, path, index);}
+	else {strcpy(temp, "");}
+	return temp;
+}
+Node * traverseDir(char *name){
 	Node *child = cwd->Child;
 	while(child){
 		if(child->Type == 'D' && !strcmp(child->Name, name)){
@@ -94,39 +118,59 @@ Node * locateDir(char *name){
 	}
 	return NULL;
 }
-bool tokenize(char *path)
+Node * locateDir2(char *path)
 {
-	char* delimiter = "/", tempPath[64];
-	strcpy(tempPath, pathname);
+	char* delimiter = "/";
 	char *token = strtok(path, delimiter); // first call to strtok()
-	Node * cur = cwd;
+	Node *cur = cwd;
 	while(token){
+		puts(token);
 		if(!strcmp(token, "..") && cwd != root){
 			cur = cur->Parent;
-			char * substr = strrchr(tempPath, '/');
-			int n = strlen(tempPath)-strlen(substr);
-			tempPath[n] = '\0';
 		}
-		else if((cur =locateDir(token)) == NULL){ //If invalid directory name
-			return false;
-		}
-		else{
-			strcat(tempPath, "/");
-			strcat(tempPath, cur->Name);
+		else if((cur=traverseDir(token)) == NULL){ //If invalid directory name
+			return NULL;
 		}
 		token = strtok(NULL, delimiter); // call strtok() until it returns NULL
 	}
-	cwd = cur;
-	strcpy(pathname, tempPath);
-	return true;
+	return cur;
+}
+Node *locateDir(char *path)
+{
+	char* delimiter = "/";
+	char *token = strtok(path, delimiter); // first call to strtok()
+	Node * cur = cwd;
+	while(token){
+		printf("Token: '%s'\n", token);
+		if(!strcmp(token, "..") && cwd != root){
+			cur = cur->Parent;
+			char * substr = getBaseName(cwPath);
+			int n = strlen(cwPath)-strlen(substr);
+			cwPath[n] = '\0';
+		}
+		else if((cur=traverseDir(token)) == NULL){ //If invalid directory name
+			return false;
+		}
+		else{
+			strcat(cwPath, "/");
+			strcat(cwPath, cur->Name);
+		}
+		token = strtok(NULL, delimiter); // call strtok() until it returns NULL
+	}
+	return cur;
 }
 void dbname(char *path){
-	char temp[128]; // dirname(), basename() destroy original pathname
-	strcpy(temp, path);
-	strcpy(dname, dirname(temp));
-	strcpy(temp, path);
-	strcpy(bname, basename(temp));
-	if(!strcmp(dname, ".")){strcpy(dname, "");}
+		char temp[128]; // dirname(), basename() destroy original pathname
+		strcpy(temp, path);
+		strcpy(dname, dirname(temp));
+		strcpy(temp, path);
+		strcpy(bname, basename(temp));
+		strcpy(path, "");
+		//set bname or dname to the empty string if they are equal to '.'
+		if(strlen(dname) == 1 && (*dname) == '.'){strcpy(dname, ""); }
+		else{ strcat(path, dname); strcat(path, "/"); }
+		if(strlen(bname) == 1 && (*bname) == '.'){strcpy(bname, ""); }
+		else { strcat(path, bname); }
 }
 int findCmd(char *command)
 {
@@ -136,69 +180,78 @@ int findCmd(char *command)
 			return i; // found command: return index i
 		} i++;
 	}
-	return -1; // not found: return -1
+	return -1; // not found: ccdreturn -1
 }
 Node * createNode(char* name, char type){
 	Node *newNode = (Node *) malloc(sizeof(Node));
 	strcpy(newNode->Name, name);
 	newNode->Type = type;
-	newNode->Sibling = newNode->Parent = NULL;
+	newNode->Sibling = newNode->Parent = newNode->Child = NULL;
 	return newNode;
 }
+void displayNode(Node *node){
+	puts("***** Node *****");
+	printf("Name: %s\nType: %c\n", node->Name, node->Type);
+	char *parent = node->Parent ? node->Parent->Name : "N/A";
+	char *sibling = node->Sibling ? node->Sibling->Name : "N/A";
+	char *child = node->Child ? node->Child->Name : "N/A";
+	printf("Parent: %s\nSibling: %s\nChild: %s\n", parent, sibling, child);
+}
 int mkdir(char *name){
-	Node *cur = cwd;
+	Node *prev = NULL, *cur = cwd->Child;
 	while(cur){
 		if(cur->Type == 'D' && !strcmp(cur->Name, name)){
 			printf("mkdir: cannot create directory '%s': File exists\n", name);
 			return false;
-		}
+		}prev = cur;
 		cur = cur->Sibling;
 	}
 	Node *newDir = createNode(name, 'D');
-	newDir->Sibling = cwd->Child ? cwd->Child : NULL;
 	newDir->Parent = cwd;
-	cwd->Child = newDir;
+	if(prev) {prev->Sibling = newDir;}
+	else {cwd->Child = newDir;}
 	return true;
 }
 int rmdir(char *name){
-	Node *prev = NULL, *cur = cwd;
+	Node *prev = NULL, *cur = cwd->Child;
 	while(cur){
 		if(cur->Type == 'D' && !strcmp(cur->Name, name)){
 			if(cur == cwd->Child){ cwd->Child = cur->Sibling; }
 			else if(prev != NULL){ prev->Sibling = cur->Sibling; }
 			free(cur);
 			return true;
-		}
-		prev = cur;
+		} prev = cur;
 		cur = cur->Sibling;
 	}
 	printf("rmdir: failed to remove '%s': No such file or directory\n", name);
 	return false;
 }   
 int ls(char *name){
-	Node *sibling = cwd->Child;
-	//bool displayAll = (name == NULL || !strcmp(name, "")) ? true : false;
-	for(int i = 1; sibling; i++){
-		if(true){
-			if(i!=5){printf("%s (%c)\t", sibling->Name, sibling->Type);}
-			else{ i==0; printf("\n%s (%c)", sibling->Name, sibling->Type);}
-		}
-		sibling = sibling->Sibling;
-	}puts("");
+	if(!(cwd = locateDir(name))){
+		printf("ls: cannot access '%s': No such file or directory\n", name);
+		return false;
+	}
+	if(cwd->Type == 'D'){
+		Node *cur = cwd->Child;
+		for(int i = 1; cur; i++){
+			if(true){
+				if(i!=5){printf("%s (%c)\t", cur->Name, cur->Type);}
+				else{ i==0; printf("\n%s (%c)", cur->Name, cur->Type);}
+			}
+			cur = cur->Sibling;
+		}puts("");
+	} else{ printf("%s (%c)\n", cwd->Name, cwd->Type); }
 	return true;
 }
 int cd(char *name){
-	return tokenize(name);
-	/*Node *cur = cwd;
-	while(cur){
-		if(cur->Type == 'D' && !strcmp(cur->Name, name)){
-			cwd = cur->Child;
-			return true;
-		}
-		cur = cur->Sibling;
+	if((cwd = locateDir(bname)) != NULL){
+		strcpy(pathname, cwPath);
+		start = cwd;
+		return true;
 	}
+	strcat(cwPath, "/"); strcat(cwPath, name);
+	printf("bash: cd: %s: No such file or directory\n", cwPath);
 	return false;
-	*/
 }
 int pwd(char *name){
 	/*Start from CWD, implement pwd by recursion:
