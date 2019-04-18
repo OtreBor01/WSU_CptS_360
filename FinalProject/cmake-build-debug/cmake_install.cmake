b3e377d75580a98e28000c26a86d6f5fe8c1d5f4
ntaylor <ntaylor@eecs.wsu.edu>
2019-04-18 13:53:08 -0700

    This should help merge issues

--- FinalProject/cmake-build-debug/cmake_install.cmake
+++ FinalProject/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/austinmm/Desktop/CS360/FinalProject
+# Install script for directory: /home/noah/Documents/cpts360/Project/FinalProject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/austinmm/Desktop/CS360/FinalProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/noah/Documents/cpts360/Project/FinalProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
