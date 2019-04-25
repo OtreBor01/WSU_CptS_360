a228059ddf2b17f7bbc56ab75b2ea8d37c375ec8
ntaylor <ntaylor@eecs.wsu.edu>
2019-04-25 14:34:44 -0700

    forgot to add su

---
+++ FinalProject/Code/Level3/su_command.c
@@ -0,0 +1,15 @@
+//
+// Created by noah on 4/25/19.
+//
+
+#include "Level3.h"
+
+int _su(char* pathname){
+    if(_Su){
+        _Su = 0;
+    }
+    else{
+        _Su = 1;
+    }
+    return 0;
+}
